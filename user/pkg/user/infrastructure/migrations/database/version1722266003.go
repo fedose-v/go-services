e2c59d0ca690be7f7f11e36e76bb0494b79d9499
MrKoT321 <vladimir.fedoseev.2004@mail.ru>
2026-01-11 18:43:44 +0300

    user: Поправил создание бд

--- user/pkg/user/infrastructure/migrations/database/version1722266003.go
+++ user/pkg/user/infrastructure/migrations/database/version1722266003.go
@@ -30,19 +30,16 @@ func (v version1722266003) Up(ctx context.Context) error {
 	_, err := v.client.ExecContext(ctx, `
 		CREATE TABLE user
 		(
-		    user_id    VARCHAR(64)  NOT NULL,
-		    login      VARCHAR(32)  NOT NULL,
-		    status     INT          NOT NULL,
-		    email 	   VARCHAR(255),
-		    telegram   VARCHAR(255),
-		    created_at DATETIME     NOT NULL,
-		    updated_at DATETIME     NOT NULL,
-		    deleted_at DATETIME,
-		    PRIMARY KEY (user_id)
-		)
-		    ENGINE = InnoDB
-		    CHARACTER SET = utf8mb4
-		    COLLATE utf8mb4_unicode_ci
+			id         BINARY(16)   NOT NULL PRIMARY KEY,
+			login      VARCHAR(100) NOT NULL UNIQUE,
+			name       VARCHAR(255) NOT NULL,
+			email      VARCHAR(255) NOT NULL UNIQUE,
+			created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+			deleted_at DATETIME     NULL
+		) ENGINE = InnoDB
+		  DEFAULT CHARSET = utf8mb4
+		  COLLATE = utf8mb4_unicode_ci
 	`)
 	return errors.WithStack(err)
 }
